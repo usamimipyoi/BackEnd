9a236902e4daebe4a5dc8cdcd65c64b29ee26a2e
Phurich Thawornying <phurich.twy@gmail.com>
2023-10-20 13:43:57 +0700

    add activity

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { signUp, signIn} from '../controllers/auth_controller.js';
-
+import record from '../controllers/record_controller.js'
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.post("/activity", record);
 
 export default router;
