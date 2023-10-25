e443397e554f8cf598d2246c49eb10def2da77f3
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-25 15:44:02 +0700

    fix export module

--- routes/get_UserRecord_route.js
+++ routes/get_UserRecord_route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import getUserRecord from '../controllers/get_userRecord.js';
+import { getUserRecord, deleteUserRecord} from '../controllers/get_userRecord.js';
 
 const router = express.Router();
 
 router.get("/user_record", getUserRecord);
+router.get("/user_deleteRecord", deleteUserRecord);
 
 export default router;
